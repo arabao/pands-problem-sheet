b37226725c57ba7f71c94fbf334b2dfef6a04b39
Tosin <arabao@tcd.ie>
2022-02-10 22:42:14 +0000

    .\secondstring.py

--- secondstring.py
+++ secondstring.py
@@ -1,6 +1,12 @@
 # Solution to weekly Task 03
  
 # program that inputs a string and
-# Outputs second letter in reverse order
+# Outputs every second letter in reverse order
 
-#Author: Tosin Araba
+# Author: Tosin Araba 
+
+# sentence: The quick brown fox jumps over the lazy dog.
+
+inputString = input('Enter a string:')[::-2]
+print(inputString)
+
