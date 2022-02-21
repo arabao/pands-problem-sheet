5f4a9a7e278fe368c674c92e8a80cae483453d68
Tosin <arabao@tcd.ie>
2022-02-21 23:02:10 +0000

    .\collatz.py

--- collatz.py
+++ collatz.py
@@ -12,16 +12,16 @@
 
 def collatz(number):
    
-    if (number % 2 == 0):
-        print(number // 2)
-        return number // 2
+    if (number % 2 == 0): # if number is even
+        print(number // 2) # print the number divided by 2
+        return number // 2 # return the divided number
 
-    elif (number % 2 == 1):
-        result = number * 3 +1
-        print(result)
+    elif (number % 2 == 1): # otherwise if number is odd
+        result = number * 3 +1 # multiply the number by 3 and add 1
+        print(result) # print the number
         return result
 
 number = int(input("Enter a number: "))
-while number != 1:
+while number != 1: # continue the loop until 1 is returned
     number = collatz(int(number))
-    
+
